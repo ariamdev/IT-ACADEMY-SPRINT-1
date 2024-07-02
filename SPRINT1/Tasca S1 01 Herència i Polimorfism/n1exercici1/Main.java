67f6b4cf8fb114f1a93bc5db0c95be50678f26f3
Ainoaran <ainoa.aam@gmail.com>
2024-07-02 13:51:37 +0200

    Subida

---
+++ SPRINT1/Tasca S1 01 Herència i Polimorfism/n1exercici1/Main.java
@@ -0,0 +1,26 @@
+package n1exercici1;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Càrrega per l'accés a un membre estàtic:
+        Instruments.metodeEstatic();
+
+        //Càrrega per la creació de la primera instància:
+
+        InstrumentdeVent vent1 = new InstrumentdeVent("trompeta", 600);
+        InstrumentdeCorda corda1 = new InstrumentdeCorda("Guitarra", 1000);
+        InstrumentdePercussio percussio1 = new InstrumentdePercussio ("Tambor", 4000);
+
+        System.out.println(vent1);
+        System.out.println(corda1);
+        System.out.println(percussio1);
+
+        vent1.tocar();
+        corda1.tocar();
+        percussio1.tocar();
+
+    }
+
+}
