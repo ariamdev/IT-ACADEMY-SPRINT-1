44207c95885a805fad3ca0d533ca3fbc83bf8130
Ainoaran <ainoa.aam@gmail.com>
2024-07-21 10:43:26 +0200

    Update Smartphone.java

--- SPRINT1/Tasca S1 01 Herència i Polimorfism/n2exercici1/Smartphone.java
+++ SPRINT1/Tasca S1 01 Herència i Polimorfism/n2exercici1/Smartphone.java
@@ -1,16 +1,17 @@
 package n2exercici1;
 
 public class Smartphone extends Telefon implements Camera, Rellotge {
-	//implementi les interfícies "Camera" i "Rellotge".
 	
 	public Smartphone (String marca, String model) {
 		super(marca,model);
 	}
-	
+
+	@Override
 	public void fotografiar() {
 		System.out.println("S'està fent una foto");
 	}
 	
+	@Override
 	public void alarma() {
 		System.out.println("Està sonant l'alarma");
 	}
