9ad0a0a898c71b7e1e6b3152c81ea18085138dcb
Ainoaran <ainoa.aam@gmail.com>
2024-07-21 10:40:21 +0200

    Update Cotxe.java

--- SPRINT1/Tasca S1 01 Herència i Polimorfism/n1exercici2/Cotxe.java
+++ SPRINT1/Tasca S1 01 Herència i Polimorfism/n1exercici2/Cotxe.java
@@ -1,22 +1,43 @@
 package n1exercici2;
 
 public class Cotxe {
-	
-	private static final String marca = "Seat";
-	private static String model = "panda"; 
-	private final int potencia = 140; 
-		
-	
-	public Cotxe() {
-	}
-	
-	
-	static void frenar () {
-		System.out.println ( "El vehicle està frenant");
-	}
-	
-	public void accelerar() {
-		System.out.println("El vehicle està accelerant");
-	}
+
+	private static final String marca= "Seat";
+        private static String model = "panda";
+        private final int potencia; 
+
+    public Cotxe(int potencia) {
+        this.potencia = potencia;
+    }
+
+    public static String getMarca(){
+        return marca;
+    }
+
+    public static String getModel() {
+        return model;
+    }
+
+    public int getPotencia() {
+        return potencia;
+    }
+
+    public static void setModel(String model) {
+        Cotxe.model = model;
+    }
+
+    static void frenar () {
+            System.out.println ( "El vehicle està frenant");
+        }
+
+        public void accelerar() {
+            System.out.println("El vehicle està accelerant");
+        }
+
+    @Override
+    public String toString() {
+        return " Marca cotxe: " + marca + " model " + model +
+                " amb potència " + potencia + " CV.";
+    }
 
 }
