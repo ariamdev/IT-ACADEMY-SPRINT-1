cefa31facf1f668211f37fe5626296f646350378
Ainoaran <ainoa.aam@gmail.com>
2024-10-09 12:08:24 +0200

    Patterns

---
+++ SPRINT 3/Tasca S3 01 Patterns 1/n2ex1/AbstractFactory/main.java
@@ -0,0 +1,14 @@
+package n2ex1.AbstractFactory;
+
+import n2ex1.AbstractFactory.Factory.AgendaFactory;
+import n2ex1.AbstractFactory.Factory.Agenda;
+import n2ex1.AbstractFactory.Interface.PhoneNumber;
+
+import java.util.Scanner;
+
+
+public class main {
+    public static void main(String[] args) {
+    Agenda.menuAgenda();
+    }
+}
